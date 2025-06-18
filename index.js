248cf87bc007b2e57134d531909341b61ab3474c
Tyan <52327619+tianxk-hit@users.noreply.github.com>
2025-06-18 17:57:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,6 @@ let webPath = path.join(FILE_PATH, 'web');
 let botPath = path.join(FILE_PATH, 'bot');
 let subPath = path.join(FILE_PATH, 'sub.txt');
 let listPath = path.join(FILE_PATH, 'list.txt');
-let bootLogPath = path.join(FILE_PATH, 'boot.log');
 let configPath = path.join(FILE_PATH, 'config.json');
 
 // 如果订阅器上存在历史运行节点则先删除
